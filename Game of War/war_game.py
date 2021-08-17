3dc11236df3aafa0e4536a69c166a4cecb8ec1df
John Hanley <jwhanleyjr@gmail.com>
2021-08-17 14:04:03 -0400

    Added while statement, pop, and append

--- Game of War/war_game.py
+++ Game of War/war_game.py
@@ -31,12 +31,30 @@ new_deck = list(range(1,53))
 
 ## Shuffles Deck
 shuffled = random.sample(deck, len(deck))
-print (shuffled[0], shuffled[0].value)
+#print (shuffled[0], shuffled[0].value)
 ##Deals Hands
 your_hand = shuffled[::2]
 my_hand = shuffled[1::2]
 #print(shuffled)
 #print(your_hand, len(your_hand))
 #print(my_hand, len(my_hand))
-
-
+while len(my_hand) > 0 or len(your_hand) > 0:
+    your_card = your_hand.pop(0)
+    my_card = my_hand.pop(0)
+    print("You played {}. I played {}.".format(your_hand[0], my_hand[0]))
+    if your_hand[0].value > my_hand[0].value:
+        your_hand.append(my_card)
+        your_hand.append(your_card)
+        print("You Won!")
+    elif your_hand[0].value < my_hand[0].value:
+        my_hand.append(my_card)
+        my_hand.append(your_card)
+        print("I Won!")
+    else:
+        print("It's a Tie!  Let's Go To War")
+        break
+    print("I have {} cards in my deck.  You have {} cards in your deck".format(len(my_hand),len(your_hand)))
+if len(my_hand) == 0:
+    print ("You Took All Of My Cards.  You Win")
+else:
+    print ("I Took All Of Your Cards.  I Win")
